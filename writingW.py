f0c8c2e277d5b138163a3f56d98d6b92e44c0a2e
jonathanlin1125 <jonathan.lin1125@gmail.com>
2019-04-08 15:15:08 -0700

    Writing bot

--- writingW.py
+++ writingW.py
@@ -25,12 +25,6 @@ options = webdriver.ChromeOptions()
 #options.add_argument("headless")
 driver = webdriver.Chrome(executable_path= chrome_driver_path, options= options)
 
-gpa_scale = {"A+": 4.0, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0, "C-": 1.7, "D+": 1.3, "D": 1.0, "D-": 0.7, "F": 0.0}
-school_classes = ["I&C SCI", "MATH", "IN4MATX", "COMPSCI", "STATS"]
-ics_majors = ["Business Information Management", "Computer Science", "Computer Game Science", "Computer Science and Engineering", "Data Science", "Software Engineering", "Informatics"]
-ics_minors = ["Bioinformatics", "Digital Information Systems", "Health Informatics", "Informatics", "Information and Computer Science", "Statistics"]
-pnp_classes = ["I&CSCI 90"]
-
 """Driver functions
 """
 def login(username, password):
@@ -38,7 +32,7 @@ def login(username, password):
 	Driver logs into WebAdmin.
 	"""
 	driver.get('https://www.reg.uci.edu')
-	studentAccess = driver.find_element_by_link_text("StudentAccess")
+	studentAccess = driver.find_element_by_link_text("WebAdmin")
 	studentAccess.click()
 
 	login = driver.find_element_by_link_text("Click here to Login.")
@@ -103,31 +97,24 @@ def download_degree_source():
 def lower_writing_complete(page_source):
 	"""
 	Given a page source of degreeworks
-	Returns a list of courses student took to complete major.
+	Returns whether student finished their lower division writing requirements.
 	"""
 	soup = BeautifulSoup(page_source, features="html.parser")
 	sections = soup.body.find_all('table', attrs={'border': "0"})
 
-	if major in ics_majors:
+	try:
+
 		for section in sections:
-			try:
-				major_found = section.find('td', attrs={'class': "BlockHeadTitle"}).text.strip()[9:]
-				if major_found in ics_majors and major_found == major:
-					course_sections = section.find_all('tr', attrs={'class': re.compile(r'bgLight(100|98|0)')})
-					for course_section in course_sections:
-						try:
-							courses = course_section.find_all('tr', attrs={'class': 'CourseAppliedRowWhite'})
-							for course in courses:
-								course_title = course.find('td', attrs={'class': "CourseAppliedDataDiscNum"}).text.strip()
-								course_grade = course.find('td', attrs={'class': 'CourseAppliedDataGrade'}).text.strip()
-								if "(T)" not in course_title and course_grade != "T" and course_grade != "IP":
-									list_courses.add((course_title, course_grade))
-						except:
-							pass
-					break
-			except:
-				pass
-	return list_courses
+			block_name = section.find('td', attrs={'class': "BlockHeadTitle"}).text.strip()
+			if (block_name == "General Education Requirements"):
+				ge_sections = block_name.find_all('tr', attrs={'class': 'bgLight0'})
+				for ge in ge_sections:
+					title = ge.find('td', attrs={'class': 'RuleLabelTitleNotNeeded'})
+					if title == "I. Lower-Division Writing (minimum grade C)":
+						return False
+	except:
+		pass
+	return True
 
 def get_credentials():
 	"""
@@ -145,8 +132,26 @@ def retrieve_information():
 	username, password = get_credentials()
 	login(username, password)
 
-	view_degree_works()
-	page_source = download_degree_source()
+	exists = os.path.isfile(input_file_path)
+	if exists:
+		file = open(input_file_path, "r")
+		log = open(log_file_path, "w")
+		output = open(output_file_path, "w")
+
+		list_id = file.read().split("\n")
+		for id_num in list_id:
+			enter_id(id_num.strip())
+			view_degree_works()
+			page_source = download_degree_source()
+
+			lwd_complete = lower_writing_complete(page_source)
+
+			log.write(id_num + "\n" + str(lwd_complete) + "\n\n")
+			if not lwd_complete:
+				output.write(id_num "\n")
+
+
+	print(lower_writing_complete(page_source))
 
 if __name__ == "__main__":
 	retrieve_information()

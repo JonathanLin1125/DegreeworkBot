4ec2ebdae78a22cfe8f08932d86c0d320c782ad8
jonathanlin1125 <jonathan.lin1125@gmail.com>
2019-04-22 15:39:27 -0700

    Update

--- gradW.py
+++ gradW.py
@@ -30,7 +30,7 @@ driver = webdriver.Chrome(executable_path= chrome_driver_path, options= options)
 gpa_scale = {"A+": 4.0, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0, "C-": 1.7, "D+": 1.3, "D": 1.0, "D-": 0.7, "F": 0.0}
 school_classes = ["I&C SCI", "MATH", "IN4MATX", "COMPSCI", "STATS"]
 ics_majors = ["Business Information Management", "Computer Science", "Computer Game Science", "Computer Science and Engineering", "Data Science", "Software Engineering", "Informatics"]
-ics_minors = ["Bioinformatics", "Digital Information Systems", "Health Informatics", "Informatics", "Information and Computer Science", "Statistics"]
+ics_minors = ["Bioinformatics", "Digital Information Systems", "Health Informatics", "Informatics", "Information and Computer Science", "Information & Computer Sci", "Statistics"]
 pnp_classes = ["I&CSCI 90"]
 
 """Driver functions
